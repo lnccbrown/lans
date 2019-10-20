0df2874a89300f6f472199464405ac01003d15aa
lakshmi <krg.lakshminarasimhan@gmail.com>
2019-10-20 18:02:33 -0400

    mod config

--- src/train_detector.py
+++ src/train_detector.py
@@ -23,144 +23,6 @@ class cnn_model_struct:
     def __contains__(self, item):
         return hasattr(self,item)
 
-    # def build(self, patch, output_shape,train_mode=None):
-    #
-    #     print ("building the network")
-    #     input_patch = tf.identity(patch,name="input_patch")
-    #     with tf.name_scope('reshape'):
-    #         x_image = tf.reshape(input_patch, [-1, 416, 416, 3])
-    #
-    #     # conv layer 1
-    #     with tf.name_scope('conv1'):
-    #         self.W_conv1 = self.weight_variable([3, 3, 3, 16],var_name='wconv1')
-    #         self.b_conv1 = self.bias_variable([16],var_name='bconv1')
-    #         #if train_mode:
-    #         self.norm1 = tf.layers.batch_normalization(self.conv2d(x_image, self.W_conv1,stride=[1,1,1,1]) + self.b_conv1,scale=True,center=True,training=train_mode)
-    #         self.h_conv1 = tf.nn.leaky_relu(self.norm1, alpha=0.1)
-    #         #else:
-    #         #    self.h_conv1 = tf.nn.leaky_relu(self.conv2d(x_image, self.W_conv1,stride=[1,1,1,1]) + self.b_conv1, alpha=0.1)
-    #
-    #     # Pooling layer - downsamples by 2X.
-    #     with tf.name_scope('pool1'):
-    #         self.h_pool1 = self.max_pool_2x2(self.h_conv1)
-    #
-    #     # conv layer 2 -- maps 16 feature maps to 32.
-    #     with tf.name_scope('conv2'):
-    #         self.W_conv2 = self.weight_variable([3, 3, 16, 32],var_name='wconv2')
-    #         self.b_conv2 = self.bias_variable([32],var_name='bconv2')
-    #         #if train_mode:
-    #         self.norm2 = tf.layers.batch_normalization(self.conv2d(self.h_pool1, self.W_conv2, stride=[1, 1, 1, 1]) + self.b_conv2,scale=True,center=True,training=train_mode)
-    #         self.h_conv2 = tf.nn.leaky_relu(self.norm2, alpha=0.1)
-    #         #else:
-    #         #    self.h_conv2 = tf.nn.leaky_relu(
-    #         #        self.conv2d(self.h_pool1, self.W_conv2, stride=[1, 1, 1, 1]) + self.b_conv2, alpha=0.1)
-    #
-    #     # Second pooling layer.
-    #     with tf.name_scope('pool2'):
-    #         self.h_pool2 = self.max_pool_2x2(self.h_conv2)
-    #
-    #     # conv layer 3 -- maps 32 feature maps to 64.
-    #     with tf.name_scope('conv3'):
-    #         self.W_conv3 = self.weight_variable([3, 3, 32, 64],var_name='wconv3')
-    #         self.b_conv3 = self.bias_variable([64],var_name='bconv3')
-    #
-    #         #if train_mode:
-    #         self.norm3 = tf.layers.batch_normalization(self.conv2d(self.h_pool2, self.W_conv3, stride=[1, 1, 1, 1]) + self.b_conv3, scale=True,center=True,training=train_mode)
-    #         self.h_conv3 = tf.nn.leaky_relu(self.norm3,alpha=0.1)
-    #         #else:
-    #         #    self.h_conv3 = tf.nn.leaky_relu(
-    #         #        self.conv2d(self.h_pool2, self.W_conv3, stride=[1, 1, 1, 1]) + self.b_conv3, alpha=0.1)
-    #     # Second pooling layer.
-    #     with tf.name_scope('pool3'):
-    #         self.h_pool3 = self.max_pool_2x2(self.h_conv3)
-    #
-    #     # conv layer 4 -- maps 64 feature maps to 128.
-    #     with tf.name_scope('conv4'):
-    #         self.W_conv4 = self.weight_variable([3, 3, 64, 128],var_name='wconv4')
-    #         self.b_conv4 = self.bias_variable([128],var_name='bconv4')
-    #         #if train_mode:
-    #         self.norm4 = tf.layers.batch_normalization(self.conv2d(self.h_pool3, self.W_conv4, stride=[1, 1, 1, 1]) + self.b_conv4, scale=True,center=True,training=train_mode)
-    #         self.h_conv4 = tf.nn.leaky_relu(self.norm4, alpha=0.1)
-    #         #else:
-    #         #    self.h_conv4 = tf.nn.leaky_relu(
-    #         #        self.conv2d(self.h_pool3, self.W_conv4, stride=[1, 1, 1, 1]) + self.b_conv4, alpha=0.1)
-    #
-    #     # Second pooling layer.
-    #     with tf.name_scope('pool4'):
-    #         self.h_pool4 = self.max_pool_2x2(self.h_conv4)
-    #
-    #     # conv layer 5 -- maps 128 feature maps to 256.
-    #     with tf.name_scope('conv5'):
-    #         self.W_conv5 = self.weight_variable([3, 3, 128, 256],var_name='wconv5')
-    #         self.b_conv5 = self.bias_variable([256],var_name='bconv5')
-    #         #if train_mode:
-    #         self.norm5 = tf.layers.batch_normalization(self.conv2d(self.h_pool4, self.W_conv5, stride=[1, 1, 1, 1]) + self.b_conv5, scale=True,center=True,training=train_mode)
-    #         self.h_conv5 = tf.nn.leaky_relu(self.norm5, alpha=0.1)
-    #         #else:
-    #         #    self.h_conv5 = tf.nn.leaky_relu(
-    #         #        self.conv2d(self.h_pool4, self.W_conv5, stride=[1, 1, 1, 1]) + self.b_conv5, alpha=0.1)
-    #     # Second pooling layer.
-    #     with tf.name_scope('pool5'):
-    #         self.h_pool5 = self.max_pool_2x2(self.h_conv5)
-    #
-    #     # conv layer 6 -- maps 32 feature maps to 64.
-    #     with tf.name_scope('conv6'):
-    #         self.W_conv6 = self.weight_variable([3, 3, 256, 512],var_name='wconv6')
-    #         self.b_conv6 = self.bias_variable([512],var_name='bconv6')
-    #
-    #         #if train_mode:
-    #         self.norm6 = tf.layers.batch_normalization(self.conv2d(self.h_pool5, self.W_conv6, stride=[1, 1, 1, 1]) + self.b_conv6, scale=True,center=True,training=train_mode)
-    #         self.h_conv6 = tf.nn.leaky_relu(self.norm6,alpha=0.1)
-    #         #else:
-    #         #    self.h_conv6 = tf.nn.leaky_relu(
-    #         #        self.conv2d(self.h_pool5, self.W_conv6, stride=[1, 1, 1, 1]) + self.b_conv6, alpha=0.1)
-    #
-    #     # Second pooling layer.
-    #     with tf.name_scope('pool6'):
-    #         self.h_pool6 = self.max_pool_2x2_1(self.h_conv6)
-    #
-    #     # # conv layer 7 -- 512 to 1024
-    #     # with tf.name_scope('conv7'):
-    #     #     self.W_conv7 = self.weight_variable([3, 3, 512, 1024],var_name='wconv7')
-    #     #     self.b_conv7 = self.bias_variable([1024],var_name='bconv7')
-    #     #     #if train_mode:
-    #     #     self.norm7 = tf.layers.batch_normalization(self.conv2d(self.h_pool6, self.W_conv7,stride=[1,1,1,1]) + self.b_conv7, scale=True,center=True,training=train_mode)
-    #     #     self.h_conv7 = tf.nn.leaky_relu(self.norm7, alpha=0.1)
-    #     #     #else:
-    #     #     #    self.h_conv7 = tf.nn.leaky_relu(self.conv2d(self.h_pool6, self.W_conv7,stride=[1,1,1,1]) + self.b_conv7, alpha=0.1)
-    #     #
-    #     # # conv layer 8 -- 1024 to 1024
-    #     # with tf.name_scope('conv8'):
-    #     #     self.W_conv8 = self.weight_variable([3, 3, 1024, 1024],var_name='wconv8')
-    #     #     self.b_conv8 = self.bias_variable([1024],var_name='bconv8')
-    #     #     #if train_mode:
-    #     #     self.norm8 = tf.layers.batch_normalization(self.conv2d(self.h_conv7, self.W_conv8,stride=[1,1,1,1]) + self.b_conv8, scale=True,center=True,training=train_mode)
-    #     #     self.h_conv8 = tf.nn.leaky_relu(self.norm8, alpha=0.1)
-    #     #     #else:
-    #     #     #    self.h_conv8 = tf.nn.leaky_relu(self.conv2d(self.h_conv7, self.W_conv8,stride=[1,1,1,1]) + self.b_conv8, alpha=0.1)
-    #     #
-    #     # # conv layer 9 -- 1x1 conv for the output
-    #     # with tf.name_scope('conv9'):
-    #     #     self.W_conv9 = self.weight_variable([1, 1, 1024, 5],var_name='wconv9')
-    #     #     self.b_conv9 = self.bias_variable([5],var_name='bconv9')
-    #     #     #self.h_conv9 = tf.nn.relu(self.conv2d(self.h_conv8, self.W_conv9,stride=[1,1,1,1]) + self.b_conv9)
-    #     #     self.h_conv9 = self.conv2d(self.h_conv8, self.W_conv9, stride=[1, 1, 1, 1]) + self.b_conv9
-    #     #
-    #     #     #self.h_conv9 = tf.matmul(self.h_conv8, self.W_conv9) + self.b_conv9
-    #     #     # if train_mode:
-    #     #     #     self.h_conv9 = tf.layers.batch_normalization(self.conv2d(self.h_conv8, self.W_conv9, stride=[1, 1, 1, 1]) + self.b_conv9, scale=True,center=True,training=True)
-    #     #     # else:
-    #     #     #     self.h_conv9 = self.conv2d(self.h_conv8, self.W_conv9, stride=[1, 1, 1, 1]) + self.b_conv9
-    #     # self.output = tf.identity(self.h_conv9,name="output")
-    #
-    #     # conv layer 9 -- 1x1 conv for the output
-    #     with tf.name_scope('conv7'):
-    #         self.W_conv7 = self.weight_variable([1, 1, 512, 3],var_name='wconv7')
-    #         self.b_conv7 = self.bias_variable([3],var_name='bconv7')
-    #         self.h_conv7 = self.conv2d(self.h_pool6, self.W_conv7, stride=[1, 1, 1, 1]) + self.b_conv7
-    #
-    #     self.output = tf.identity(self.h_conv7,name="output")
-
     def build(self, patch, output_shape,train_mode=None):
 
         print ("building the network")
@@ -295,6 +157,7 @@ def calc_error(labels,predictions):
     return tf.reduce_mean(tf.sqrt(tf.reduce_sum(tf.square(labels-predictions), 1)),0)
 
 def train_model(config):
+    '''
     train_data = os.path.join(config.tfrecord_dir, config.train_tfrecords)
     val_data = os.path.join(config.tfrecord_dir, config.val_tfrecords)
 
@@ -311,7 +174,8 @@ def train_model(config):
                                         image_target_size=config.image_target_size,
                                         label_shape=config.label_shape,
                                         batch_size=config.val_batch)
-
+    '''
+    import ipdb; ipdb.set_trace()
     with tf.device('/gpu:0'):
         with tf.variable_scope("model") as scope:
             print ("creating the model")
