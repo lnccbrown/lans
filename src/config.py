e4d2e2abf6356b8b354814a4cecfb67489c056f9
Lakshmi <krg.lakshminarasimhan@gmail.com>
2019-10-22 19:20:38 -0400

    tfr write works

--- src/config.py
+++ src/config.py
@@ -7,7 +7,11 @@ class Config(object):
         # Directory setup
         self.base_dir = '/media/data_cifs/lakshmi/projectABC/'
         self.data_dir = 'data'
-        self.dataset = 'dataset_parallel_*'
+        self.dataset = 'angle_binned*'
+	self.tfrecord_dir = 'tfrecords'
+	self.train_tfrecords = 'angle_train.tfrecords'
+	self.val_tfrecords = 'angle_val.tfrecords'
+	self.test_tfrecords = 'angle_test.tfrecords'
 
         # Data configuration
         # Let's say we go with NxHxWxC configuration as of now
@@ -15,6 +19,8 @@ class Config(object):
         self.output_hist_dims = [None, 1, 500, 2]
         self.results_dir = '/media/data_cifs/lakshmi/projectABC/results/'
         self.model_output = '/media/data_cifs/lakshmi/projectABC/models/cnn-v0'
+	
+	self.data_prop = {'train':0.9, 'val':0.05, 'test':0.05}
 
         # Model hyperparameters
         self.epochs = 100
